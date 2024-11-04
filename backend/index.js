4b17156dd033a16a6dd5d8ca97d20ac3351e14ae
ravikesh <ravikesh2000indurkar@gmail.com>
2024-11-04 23:48:43 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
   })
 );
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import Home from "./pages/Home/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 
 const routes = (
   <Router>
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/dashboard" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   </Router>
 );
